b1216f0e6221f9252189defe759f55b3e1ae47fd
Ottenilogic <otteni_nick@outlook.com>
2025-06-16 20:32:16 -0500

    Add files via upload

---
+++ coffee.py
@@ -0,0 +1,20 @@
+# Mr. Otteni Coffee Shop Simulator 4000
+# Copywright (c) 2025 Mr. Otteni Media, All Rights Reserved.
+
+print("Mr. Otteni Coffee Shop Simulator 4000, Versioin 1.00")
+print("Copywright (C) 2025 Mr. Otteni Media, All Rights Reserved.\n")
+print("let's collect some information before we start the game.\n") 
+
+# Get the name and shop date
+name = input("what is your name?  ")
+shop_name = input("What will the name of your coffee shop be?  ")
+
+print("\nThanks, " + name + ". Lets set some initial pricing.\n")
+
+# Get initial price of a cup of coffee.
+cup_price = input("What do you want to charge per cup of coffee? ")
+
+# Display what we have.
+print("\nGreat. Heres what we've collected so far.\n")
+print("Your name is " + name + " and you're opening " + shop_name + "!")
+print("Your first cup of coffee well sell for $" + cup_price + ".\n")
